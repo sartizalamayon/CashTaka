9c899a45dac5c2fb08c86e80a86d65505a11f9d7
ay0-0n <sartizayo.on@gmail.com>
2024-08-07 21:57:34 +0600

    Manage Agents

--- src/pages/TopupReq.jsx
+++ src/pages/TopupReq.jsx
@@ -2,7 +2,7 @@ import useAllTopupReq from "../hooks/useAllTopupReq";
 
 const TopupReq = () => {
     const [topupReq,isLoading, refetch] = useAllTopupReq()
-    console.log(topupReq)
+    
     return (
         <div>
             Topup reqs
